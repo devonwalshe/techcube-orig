902a7347301e12b876f97068daaabfefc7c5c899
Devon Walshe <devon.walshe@gmail.com>
2012-06-01 17:02:34 +0100

    removed compiled assets and set compile in production.rb environment to true

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module Tc
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile += ["main.css"]
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
