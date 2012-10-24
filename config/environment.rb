c83cc90ff3369c24b7426a7c0eb8535167409e4d
Devon Walshe <devon.walshe@gmail.com>
2012-10-24 10:31:12 +0100

    added mailer config to production environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Tc::Application.initialize!
+
+# Configuration for using SendGrid on Heroku
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#   :user_name => "yourSendGridusernameyougetfromheroku",
+#   :password => "yourSendGridpasswordyougetfromheroku",
+#   :domain => "staging.freelanceful.com",
+#   :address => "smtp.sendgrid.net",
+#   :port => 587,
+#   :authentication => :plain,
+#   :enable_starttls_auto => true
+# }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,15 @@ Tc::Application.configure do
   
   # Mailer configuration
   config.action_mailer.raise_delivery_errors = true
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => 'noreply@techcu.be',
+    :password             => 't3chcub3',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
+  
 end
