d7c8dd3a43ef79929f4abde10cdf5601e9b4d8fc
Devon Walshe <devon.walshe@gmail.com>
2012-12-05 19:27:22 +0100

    removed changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,21 +21,5 @@ class HomeController < ApplicationController
     @contact = Contact.new
   end
   
-  def news
-    @contact = Contact.new
-    @tumblr_hash = get_tumblr_articles
-    @articles = @tumblr_hash["tumblr"]["posts"]["post"] if @tumblr_hash != [] 
-  end
-  
-  private
-  
-  def get_tumblr_articles
-    uri = URI.parse("http://techcube-ed.tumblr.com/api/read")
-    http = Net::HTTP.new(uri.host, uri.port)
-    response = http.request(Net::HTTP::Get.new(uri.request_uri))
-    xml = Nokogiri::XML::Document.parse(response.body)
-    tumblr_hash = Hash.from_xml(xml.to_s)
-    return tumblr_hash
-  end
   
 end
