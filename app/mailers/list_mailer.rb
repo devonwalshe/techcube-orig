1001f02e73d338a5c01d26ce7c66521007a43969
Devon Walshe <devon.walshe@gmail.com>
2012-12-07 15:31:16 +0100

    added a thank you signup mailer and mailing list signups automatically to go mailchimp

--- app/mailers/list_mailer.rb
+++ app/mailers/list_mailer.rb
@@ -3,7 +3,11 @@ class ListMailer < ActionMailer::Base
   
   def mailing_list_notification_email(signup)
     @signup = signup
-    mail(:to => "notifications@techcu.be", :subject => "NEW MAILING LIST SIGNUP - #{@signup.email} - #{@signup.name} - is #{@signup.iama}")
-    
+    mail(:to => "notifications@techcu.be", :subject => "NEW MAILING LIST SIGNUP - #{@signup.email} - #{@signup.name} - is #{@signup.iama}")  
+  end
+  
+  def mailing_list_thankyou_email(signup)
+    @signup = signup
+    mail(:to => @signup.email, :subject => "Thank you for signing up to TechCube's mailing list")
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,16 @@
 class Contact < ActiveRecord::Base
   
+  WEBSITE_LISTS = {
+    :services => "4ba196bdab",
+    :students => "9914162b13",
+    :freelancers => "fb04a6021e",
+    :investors => "a026764a53",
+    :startups => "c71b6dcd1d",
+    :all => "90b2e3c284",
+    :test => "3b96e43454"
+  }
+  
+  
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   attr_accessible :email, :iama, :name
   
